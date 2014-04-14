5f0e945ece1f929360c7628327a8f48e4c1cbd93
Jason Lopez <AtomSoft@gmail.com>
2014-04-14 11:36:36 -0400

    first

---
+++ Image2Hex/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Image2Hex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imgMain = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmdFormat = new System.Windows.Forms.ComboBox();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgMain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgMain
+            // 
+            this.imgMain.Location = new System.Drawing.Point(12, 41);
+            this.imgMain.Name = "imgMain";
+            this.imgMain.Size = new System.Drawing.Size(480, 272);
+            this.imgMain.TabIndex = 0;
+            this.imgMain.TabStop = false;
+            this.imgMain.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.imgMain_LoadCompleted);
+            this.imgMain.SizeChanged += new System.EventHandler(this.imgMain_SizeChanged);
+            this.imgMain.Click += new System.EventHandler(this.imgMain_Click);
+            this.imgMain.MouseMove += new System.Windows.Forms.MouseEventHandler(this.imgMain_MouseMove);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 7);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open Image";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(12, 333);
+            this.txtUser.Multiline = true;
+            this.txtUser.Name = "txtUser";
+            this.txtUser.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtUser.Size = new System.Drawing.Size(966, 332);
+            this.txtUser.TabIndex = 2;
+            this.txtUser.Visible = false;
+            this.txtUser.WordWrap = false;
+            this.txtUser.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Format";
+            // 
+            // cmdFormat
+            // 
+            this.cmdFormat.FormattingEnabled = true;
+            this.cmdFormat.Items.AddRange(new object[] {
+            "RGB565"});
+            this.cmdFormat.Location = new System.Drawing.Point(290, 9);
+            this.cmdFormat.Name = "cmdFormat";
+            this.cmdFormat.Size = new System.Drawing.Size(121, 21);
+            this.cmdFormat.TabIndex = 4;
+            this.cmdFormat.Text = "RGB565";
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(93, 7);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(75, 23);
+            this.btnGen.TabIndex = 5;
+            this.btnGen.Text = "Generate";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(248, 218);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(219, 95);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 327);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.cmdFormat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.imgMain);
+            this.Name = "Form1";
+            this.Text = "AtomSoftTech - Image2Hex";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgMain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgMain;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmdFormat;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Image2Hex/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Image2Hex
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap theImage;
+        string theImageURL;
+        string theName;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            switch (cmdFormat.Text)
+            { 
+                case "RGB565":
+                    GenRGB565();
+                    break;
+            }
+
+
+        }
+        Bitmap targetBmp;
+        private void GenRGB565()
+        {
+            int IMAGE_HEIGHT = theImage.Height;
+            int IMAGE_WIDTH = theImage.Width;
+
+            int x, y;
+
+            byte redp,greenp,bluep;
+
+            int ByteCount = (IMAGE_WIDTH * IMAGE_HEIGHT) * 2;
+
+            byte[] imageBuffer = new byte[ByteCount];
+
+            int bytePOS = 0;
+            int byteINC = 2;
+
+            
+
+            Bitmap newBmp = new Bitmap(theImage);
+            targetBmp = newBmp.Clone(new Rectangle(0, 0, newBmp.Width, newBmp.Height), PixelFormat.Format16bppRgb565);
+
+            /*
+            pictureBox1.Image = targetBmp;
+
+            if (MessageBox.Show("Review Image... Continue?", "Image2Hex", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.No)
+                return;
+            */
+
+            for (y = 0; y < IMAGE_HEIGHT; y++)
+            {
+                for (x = 0; x < IMAGE_WIDTH; x++)
+                {
+                    
+                    redp = targetBmp.GetPixel(x, y).R;
+                    greenp = targetBmp.GetPixel(x, y).G;
+                    bluep = theImage.GetPixel(x, y).B;
+                    /*
+                    imageBuffer[bytePOS] = (byte)((redp << 3) | (greenp >> 5));
+                    imageBuffer[bytePOS + 1] = (byte)((greenp << 5) | bluep);
+                    */
+
+                    UInt32 retPixel = (UInt32)(redp<<16)|(UInt32)(greenp<<8)|(UInt32)(bluep);
+                    aPixel tempPixel = TF2ST(retPixel);
+
+                    imageBuffer[bytePOS] = tempPixel.uPixel;
+                    imageBuffer[bytePOS + 1] = tempPixel.lPixel;
+
+                    bytePOS += byteINC;
+                }
+            }
+            /*
+            string userData = "";
+
+            for (x = 0; x < ByteCount; x++)
+            {
+                userData += "0x" + imageBuffer[x].ToString("x") + ",";
+                if (((x+1) % 40)==0)
+                    userData += "\r\n    ";
+            }
+
+            userData = userData.Substring(0, userData.Length - 1);
+
+            string userHeader = "";
+
+            userHeader += "unsigned char "+ theName + "["+ByteCount+"] = {\r\n    "+userData+"};";
+            txtUser.Text = userHeader;
+            */
+            string newASBMP = theImageURL.Substring(0,theImageURL.Length -4).ToLower() + ".asi";
+
+            byte[] IMG_HEADER_HEIGHT = new byte[2];
+            byte[] IMG_HEADER_WIDTH = new byte[2];
+            
+            FileStream fs = new FileStream(newASBMP, FileMode.Create);
+            BinaryWriter w = new BinaryWriter(fs);
+
+            IMG_HEADER_HEIGHT[1] = (byte)(IMAGE_HEIGHT >> 8);
+            IMG_HEADER_HEIGHT[0] = (byte)(IMAGE_HEIGHT & 0xFF);
+
+            IMG_HEADER_WIDTH[1] = (byte)(IMAGE_WIDTH >> 8);
+            IMG_HEADER_WIDTH[0] = (byte)(IMAGE_WIDTH & 0xFF);
+
+            w.Write("atomsoftech asi");
+            w.Write(IMG_HEADER_HEIGHT);
+            w.Write(IMG_HEADER_WIDTH);
+            
+            w.Write(imageBuffer);
+        
+            w.Close();
+            fs.Close();
+
+            MessageBox.Show("Created: " + newASBMP);
+
+
+        }
+
+
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            dlgOpen.FileName = "";
+
+            if (dlgOpen.ShowDialog() == System.Windows.Forms.DialogResult.Cancel) return;
+
+            theImageURL = dlgOpen.FileName;
+
+            theImage = new Bitmap(theImageURL);
+
+            bool isLarger = false;
+            /*
+            if (theImage.Width > 480) isLarger = true;
+            if (theImage.Height > 272) isLarger = true;
+
+            if (isLarger == true)
+            {
+                MessageBox.Show("Image is larger than display and will be canceled.\nSize: " + theImage.Width + "x" + theImage.Height, "AtomSoftTech");
+                return;
+            }
+            */
+            imgMain.Width = theImage.Width;
+            imgMain.Height = theImage.Height;
+
+
+            imgMain.Image = theImage;
+
+            if(theImage.Width > 528)
+                this.Width = theImage.Width + 38;
+
+            if (theImage.Height > 365)
+                this.Height = theImage.Height + 90;
+
+            string[] tempArray = theImageURL.Split('\\');
+            theName = tempArray[tempArray.Length-1];
+            theName = theName.Replace(".", "_");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dlgOpen.Filter = "All Files|*.*";
+            dlgOpen.Title = "Open Image";
+
+            imgMain.SizeMode = PictureBoxSizeMode.Normal;
+            
+        }
+
+        public class aPixel
+        {
+            // Field 
+            public byte uPixel;
+            public byte lPixel;
+
+            // Constructor that takes no arguments. 
+            public aPixel()
+            {
+                uPixel = 0;
+                lPixel = 0;
+            }
+
+            // Constructor that takes one argument. 
+            public aPixel(byte anuPixel, byte anlPixel)
+            {
+                uPixel = anuPixel;
+                lPixel = anlPixel;
+            }
+
+            // Method 
+            public void aPixelSet(byte anuPixel, byte anlPixel)
+            {
+                uPixel = anuPixel;
+                lPixel = anlPixel;
+            }
+        }
+
+        private aPixel TF2ST(UInt32 inPixel)
+        {
+            aPixel outPixel = new aPixel();
+            byte upperP, lowerP;
+
+            UInt16 RED   = (UInt16)((inPixel >> 16) & 0xF8);
+            UInt16 GREEN = (UInt16)((inPixel >> 8) & 0xFC);
+            UInt16 BLUE  = (UInt16)(inPixel & 0xF8);
+
+            upperP = (byte)(RED | (GREEN >> 5));
+            lowerP = (byte)((GREEN << 3) | (BLUE >> 3));
+
+            outPixel.aPixelSet(upperP, lowerP);
+
+            return outPixel;
+        }
+
+        private void imgMain_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imgMain_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            this.Text = "Location - X: " + e.X + " Y:" + e.Y;
+        }
+
+        private void imgMain_SizeChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void imgMain_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            
+        }
+
+    }
+}
